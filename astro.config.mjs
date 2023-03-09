a2fab5a8130f19eed20a495ecaa46ab2abb588cc
rooct <896466205@qq.com>
2023-03-09 08:47:37 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ astro.config.mjs
@@ -0,0 +1,49 @@
+import { defineConfig } from 'astro/config'
+import unocss from 'unocss/astro'
+import { presetUno } from 'unocss'
+import presetAttributify from '@unocss/preset-attributify'
+import presetTypography from '@unocss/preset-typography'
+import presetIcons from '@unocss/preset-icons'
+import solidJs from '@astrojs/solid-js'
+import vercelDisableBlocks from './plugins/vercelDisableBlocks'
+
+import node from '@astrojs/node'
+import vercel from '@astrojs/vercel/edge'
+
+const envAdapter = () => {
+  if (process.env.OUTPUT == 'vercel') {
+    return vercel()
+  } else {
+    return node({
+      mode: 'standalone'
+    })
+  }
+}
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    unocss({
+      presets: [
+        presetAttributify(),
+        presetUno(),
+        presetTypography({
+          cssExtend: {
+            "ul,ol": {
+              "padding-left": "2em"
+            }
+          }
+        }),
+        presetIcons(),
+      ]
+    }),
+    solidJs()
+  ],
+  output: 'server',
+  adapter: envAdapter(),
+  vite: {
+    plugins: [
+      process.env.OUTPUT == 'vercel' && vercelDisableBlocks(),
+    ]
+  },
+});
